c3d8def3601a5263ece1d493bfa7ae16c29fd2c2
tangtantivirun <tang2tasketball@gmail.com>
2018-01-04 10:43:25 -0500

    Login Interface 1/4/18

---
+++ Kevin/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 12/7/17.
+//  Copyright © 2017 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseCore
+
+class SignUpViewController{
+    
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func createAccountAction(_ sender: Any) {
+}
+}
--- Podfile
+++ Podfile
@@ -7,4 +7,5 @@ target 'Kevin' do
 
   # Pods for Kevin
 	pod ’Firebase/Core'
+	pod ‘Firebase/Auth’
 end
