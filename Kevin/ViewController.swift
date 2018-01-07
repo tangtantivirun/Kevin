417a77ebdd8f55f117a47991578ea7dc8d151f47
tangtantivirun <tang2tasketball@gmail.com>
2018-01-06 22:30:02 -0500

    Creating Login 01/06/18

--- Kevin/ViewController.swift
+++ Kevin/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        if (Auth.auth().currentUser != nil) {
+            self.pressentLoggedInScreen()
+        }
     }
-
-
+    
+    @IBAction func createAccountAction(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password, completion: {user, error in
+                if let firebaseError = error{
+                    print(firebaseError.localizedDescription)
+                    return
+                }
+                 self.pressentLoggedInScreen()
+            })
+        }
+    }
+    @IBAction func loginTapped(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if let firebaseError = error {
+                print(firebaseError.localizedDescription)
+                return
+            }
+          self.pressentLoggedInScreen()
+        })
+        }
+    }
+    func pressentLoggedInScreen() {
+        let storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let loggedInViewController:LoggedInViewController = storyboard.instantiateViewController(withIdentifier:"LoggedInViewController") as! LoggedInViewController
+        self.present(loggedInViewController, animated: true, completion: nil)
+    }
 }
+
 
