b375b02b8d9027549450ecec9df537a828066d5b
tangtantivirun <tang2tasketball@gmail.com>
2018-01-10 10:51:17 -0500

    1/10/18

---
+++ Kevin/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 1/7/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+class HomeVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+     override func viewDidAppear(_ animated: Bool) {
+    }
+}
---
+++ Kevin/SignUpVC.swift
@@ -0,0 +1,55 @@
+//
+//  SignUpVC.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 1/7/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+class SignUpVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "< Back", style: .plain, target: self, action: #selector(backAction))
+    }
+    
+    @objc func backAction(){
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func createAccountAction(_ sender: Any) {
+        if emailTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
