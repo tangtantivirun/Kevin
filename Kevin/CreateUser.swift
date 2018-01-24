0cb0ceee77e990207c19c71b4989fbc78005d69e
tangtantivirun <tang2tasketball@gmail.com>
2018-01-24 13:37:30 -0500

    adding name component when users sign up

---
+++ Kevin/CreateUser.swift
@@ -0,0 +1,18 @@
+//
+//  CreateUser.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 1/24/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class CreateUser: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
