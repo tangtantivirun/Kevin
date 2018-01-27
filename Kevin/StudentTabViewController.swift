9f296f9b2430d4960f9a2fa5da4972ffe7894d89
tangtantivirun <tang2tasketball@gmail.com>
2018-01-27 14:35:52 -0500

    created StudentTableViewCell and organized the view controller and storyboards

---
+++ Kevin/StudentTabViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StudentTabViewController.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 1/27/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StudentTabViewController: UITableViewController {
+    
+}
---
+++ Kevin/StudentTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  StudentTableViewCell.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 1/27/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(isSelected, animated: animated)
+    }
+    
+}
