6a008b3f43aedfdefa8bc850c45a2c26363b96bc
tangtantivirun <tang2tasketball@gmail.com>
2018-01-28 00:53:11 -0500

    Tutor table view interface started

---
+++ Kevin/TutorTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TutorTableViewController.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 1/28/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TutorTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
