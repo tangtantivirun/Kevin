cd12f80804245d8e9abcd145b3ebfcaa1b2c07a4
Victoria <vrknight2000@gmail.com>
2018-02-09 20:44:24 -0500

    map kinda exists on peer tutor side and it's not crashing!!!

---
+++ Kevin/PeerTutorMapViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PeerTutorMapViewController.swift
+//  Kevin
+//
+//  Created by Victoria Knight on 2/9/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PeerTutorMapViewController: UIViewController {
+  
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let centerLocation = CLLocation(latitude: 43.1948, longitude: 71.5734)
+        
+        let regionSpan: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocation) {
+            let selectedRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionSpan, regionSpan)
+            mapView.setRegion(selectedRegion, animated: true)
+        }
+        centerMapOnLocation(location: centerLocation)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
