83c53d08ce92cfc7b0c445171fe60a964062280f
tangtantivirun <tang2tasketball@gmail.com>
2018-02-18 22:02:56 -0500

    adding username works again

--- Kevin/Login/CreateUsernameViewController.swift
+++ Kevin/Login/CreateUsernameViewController.swift
@@ -2,44 +2,41 @@
 //  CreateUsernameViewController.swift
 //  Kevin
 //
-//  Created by Tang Tantivirun on 2/2/18.
+//  Created by Tang Tantivirun on 2/18/18.
 //  Copyright © 2018 Tantivirun. All rights reserved.
 //
 
 import Foundation
-import UIKit
+import FirebaseDatabase
 import FirebaseAuth
-import FirebaseDatabase.FIRDataSnapshot
+import UIKit
 
-class CreateUsernameViewController: ViewController {
-    
+class CreateUsernameViewController: UIViewController {
+    let firUser = Auth.auth().currentUser
     @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var nextButton: UIButton!
     
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         nextButton.layer.cornerRadius = 6
     }
-    
     @IBAction func nextButtonTapped(_ sender: Any) {
-        guard let firUser = Auth.auth().currentUser,
-            let username = usernameTextField.text,
-           !username.isEmpty else {return}
+        let username = usernameTextField.text
+      
         
         let userAttrs = ["username": username]
-        let ref = Database.database().reference().child("users").child(firUser.uid)
+        let ref = Database.database().reference().child("users").child((firUser?.uid)!)
         
-        ref.setValue(userAttrs) { (error, ref) in
+        ref.setValue(userAttrs) {(error, ref) in
             if let error = error {
                 assertionFailure(error.localizedDescription)
-                return
+                return 
             }
             ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                _ = User(snapshot: snapshot)
+                _ = User(snapshot:snapshot)
             })
-        
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-        self.present(vc!, animated: true, completion: nil)
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
         }
     }
 }
