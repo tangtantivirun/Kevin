587d32007ef5e08a0b93a8dcff081b5f5cd7959e
tangtantivirun <tang2tasketball@gmail.com>
2018-02-21 02:14:20 -0500

    build interface for all requests (admin side)

---
+++ Kevin/Admin/ApprovedLocationsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ApprovedLocationsViewController.swift
+//  Kevin
+//
+//  Created by Tang Tantivirun on 2/21/18.
+//  Copyright © 2018 Tantivirun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ApprovedLocationsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "< Back", style: .plain, target: self, action: #selector(backAction))
+    }
+    @objc func backAction(){
+        //print("Back Button Clicked")
+        dismiss(animated: true, completion: nil)
+    }
+}
